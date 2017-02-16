7fb3382588fd0e7d6f1c6d13552a1b00ad1592cf
fatong <1772242203@qq.com>
2017-02-16 09:32:46 +0800

    魏发同 前台分类相关提交

--- laravel52/app/Http/Controllers/MarketController.php
+++ laravel52/app/Http/Controllers/MarketController.php
@@ -8,13 +8,21 @@ use App\User;
 class MarketController extends Controller{
 
 /**
- * 商品
+ * 全部课程分类
  * @return [type] [description]
  */
     public function market(){
-        return view('market.market');
+        $types = DB::table('study_type')->where('type_is_show',1)->where('parent_id','=',0)->get();
+        //print_r($types);die;
+        foreach($types as $key=>$val){
+            $val->pp=DB::table('study_type')->where('type_is_show',1)->where('parent_id','=',$val->type_id)->get();
+            foreach($val->pp as $k=>$v){
+                $v->pp=DB::table('study_type')->where('type_is_show',1)->where('parent_id','=',$v->type_id)->get();
+            }
+        }
+        //print_r($types);die;
+        return view('market.market',['types'=>$types]);
     }
-
 
 
 
