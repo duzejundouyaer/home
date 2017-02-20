a7233aa2126b44ee6182919917ddaa69bf701a6d
hwj <997242980@qq.com>
2017-02-20 19:17:51 +0800

    黄文靖提交

--- laravel52/app/User.php
+++ laravel52/app/User.php
@@ -6,8 +6,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    protected $table='user';
-    protected $primaryKey='id';
+    protected $table='study_user';
+    protected $primaryKey='user_id';
+    public $timestamps = false;
     /**
      * The attributes that are mass assignable.
      *
@@ -25,4 +26,22 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * 修改个人资料
+     */
+    public function Update_info($nickname,$nickName,$desc)
+    {
+        $arr = $this->where('nickname','=',$nickname)->first();
+        $res = $this->where('user_id','=',$arr['user_id'])->update(['nickname'=>$nickName,'user_desc'=>$desc]);
+        return 1;
+    }
+    /**
+     * 展示用户个人资料
+     */
+    public function infomation($nickname)
+    {
+        $reg = $this->where('nickname','=',$nickname)->first();
+        return $reg;
+    }
 }
