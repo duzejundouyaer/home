aa219b23a2971409f1369685d5343773a24d3fd7
hwj <997242980@qq.com>
2017-02-21 16:59:01 +0800

    黄文靖提交

--- laravel52/app/Http/routes.php
+++ laravel52/app/Http/routes.php
@@ -38,6 +38,7 @@ Route::get('cont','MarketController@cont');//课程详情
 Route::get('bfang','MarketController@bfang');//课程详情
 
 Route::post('shopcart','MarketController@shopcart');//加入购物车
+Route::get('select_cart','MarketController@getCart');//查看用户是否已经将视频添加到购物车
 
 Route::get('more','MoreController@more');//更多
 
