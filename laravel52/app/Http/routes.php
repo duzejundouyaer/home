61992291b8d0b05cb0e44499c376988165a66ade
hwj <997242980@qq.com>
2017-02-22 08:34:55 +0800

    黄文靖提交

--- laravel52/app/Http/routes.php
+++ laravel52/app/Http/routes.php
@@ -33,6 +33,8 @@ Route::get('del_cart','CenterController@delCart');//删除购物车信息
 Route::get('myorder','CenterController@order_list');//个人订单列表
 Route::get('feedback','CenterController@user_Feed');//反馈界面
 Route::post('sub_feed','CenterController@subFeed');//反馈提交
+Route::get('collection','CenterController@my_collection');//添加个人收藏
+
 
 Route::get('market','MarketController@market');//全部课程分类
 Route::get('curr','MarketController@curr');//全部课程分类
